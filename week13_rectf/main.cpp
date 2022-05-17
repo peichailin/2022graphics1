c7af78923ef26023b7b3fd23f66a4d78d8fab011
peichailin <09160163@me.mcu.edu.tw>
2022-05-17 21:50:23 +0800

    week13

---
+++ week13_rectf/main.cpp
@@ -0,0 +1,19 @@
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <GL/glut.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); ///畫圖前,清畫面
+    glRectf(0.3,0.5,-0.3,-0.5);
+    glutSwapBuffers(); ///畫好後,交換出來
+}
+int main(int argc,char**argv) ///main()主函式進階版
+{
+    glutInit(&argc,argv); ///把參數給glutInit初始化
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH); ///雙緩衝區,3D深度功能
+    glutCreateWindow("第13的程式"); ///開啟GLUT視窗
+    glutDisplayFunc(display); ///用來顯示函式
+
+    glutMainLoop();
+}
